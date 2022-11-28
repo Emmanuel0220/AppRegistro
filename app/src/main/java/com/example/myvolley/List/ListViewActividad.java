f170cc5d4978d381aadaa0f5bd47e4b34794f5b0
Emmanuel0220 <aguilera.garcia.christopher.e@gmail.com>
2022-11-27 22:33:25 -0600

    Nueva versión

---
+++ app/src/main/java/com/example/myvolley/List/ListViewActividad.java
@@ -0,0 +1,16 @@
+package com.example.myvolley.List;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myvolley.R;
+
+public class ListViewActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_actividad);
+    }
+}
